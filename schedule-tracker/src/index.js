14adb3529ded237acb11b2d04c3c9dc62b9049c2
Yuvaraj <yuvarajpuggi19@gmail.com>
2025-12-20 07:06:19 +0530

    view data update

--- schedule-tracker/src/index.js
+++ schedule-tracker/src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
+import { Analytics } from '@vercel/analytics/react';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
 
